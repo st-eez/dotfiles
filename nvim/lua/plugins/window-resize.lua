0af33aa024671c593d8377db2c33b75b2d8636da
st-eez <stevedimakos97@gmail.com>
2025-11-10 19:11:38 -0500

    Replace Hyper key with Ctrl+Opt+arrows for Neovim window resize
    - Change from Hyper+=/- to Ctrl+Opt+arrows for all 4 directions
    - Disable all native Ctrl+arrow bindings (reserved for Aerospace)
    - Update Raycast keybind extension to match new bindings

--- nvim/lua/plugins/window-resize.lua
+++ nvim/lua/plugins/window-resize.lua
@@ -2,21 +2,33 @@ return {
   {
     "LazyVim/LazyVim",
     keys = function(_, keys)
-      local del = { "<C-Left>", "<C-Right>" }
+      -- Disable native Ctrl+arrow bindings (reserved for Aerospace)
+      local del = { "<C-Up>", "<C-Down>", "<C-Left>", "<C-Right>" }
       for _, lhs in ipairs(del) do
         table.insert(keys, { lhs, false })
       end
 
+      -- Use Ctrl+Opt+arrows for window resizing
       local map = {
         {
-          "<D-C-M-S-->",
+          "<C-A-Left>",
           "<cmd>vertical resize -5<cr>",
-          desc = "Hyper Shrink Window Width",
+          desc = "Decrease Window Width",
         },
         {
-          "<D-C-M-S-=>",
+          "<C-A-Right>",
           "<cmd>vertical resize +5<cr>",
-          desc = "Hyper Grow Window Width",
+          desc = "Increase Window Width",
+        },
+        {
+          "<C-A-Down>",
+          "<cmd>resize -5<cr>",
+          desc = "Decrease Window Height",
+        },
+        {
+          "<C-A-Up>",
+          "<cmd>resize +5<cr>",
+          desc = "Increase Window Height",
         },
       }
       vim.list_extend(keys, map)
