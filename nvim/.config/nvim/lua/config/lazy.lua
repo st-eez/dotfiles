fc95961ebb59984d1e1794dbcd2f0e17121a04ea
st-eez <stevedimakos97@gmail.com>
2026-01-02 10:24:45 -0500

    feat(nvim): add local dev path for plugin development

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -15,6 +15,10 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
+  dev = {
+    path = "~/Projects/Personal",
+    fallback = true,
+  },
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
