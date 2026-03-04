4793edf0f720abbe43b2f3dff538527ae9f4c738
steez <stevedimakos97@gmail.com>
2026-03-04 13:50:35 -0500

    fix: retune vantasteez palette and nvim warning highlights

--- themes/configs/vantasteez/neovim.lua
+++ themes/configs/vantasteez/neovim.lua
@@ -18,35 +18,54 @@ return {
           colors.bg_dark = "#0d0d0d"
           colors.bg_dark1 = "#0d0d0d"
           colors.bg_highlight = "#1a1a1a"
-          colors.blue0 = "#2d3448"
-          colors.blue7 = "#202535"
+          colors.fg = "#eaeaea"
+          colors.fg_dark = "#d6d6d6"
+          colors.comment = "#8a8a8a"
 
-          colors.fg = "#f2f2f2"
-          colors.fg_dark = "#d8d8d8"
-          colors.fg_gutter = "#555555"
-          colors.comment = "#8d8d8d"
-          colors.dark3 = "#5f5f5f"
-          colors.dark5 = "#8a8a8a"
-          colors.terminal_black = "#505050"
+          -- Keep Vantablack mapping semantics, swap in Vantasteez canonical palette.
+          colors.red = "#c995a0"
+          colors.red1 = "#c995a0"
+          colors.orange = "#d4ad86"
+          colors.yellow = "#d8c08a"
+          colors.green = "#a3c8a0"
+          colors.green1 = "#a3c8a0"
+          colors.green2 = "#a3c8a0"
+          colors.cyan = "#8fbfc8"
+          colors.teal = "#8fbfc8"
+          colors.blue = "#8fa8d8"
+          colors.blue2 = "#8fbfc8"
+          colors.magenta = "#b9a3d7"
+          colors.magenta2 = "#b9a3d7"
+          colors.purple = "#b9a3d7"
 
-          colors.red = "#d68392"
-          colors.red1 = "#e09dac"
-          colors.orange = "#dc9d77"
-          colors.yellow = "#c8a87a"
-          colors.green = "#9dbc7b"
-          colors.green1 = "#a8c48b"
-          colors.green2 = "#a8c48b"
-          colors.cyan = "#87bcdc"
-          colors.teal = "#87bcdc"
-          colors.blue = "#859fd6"
-          colors.blue1 = "#9bb0e0"
-          colors.blue2 = "#87bcdc"
-          colors.blue5 = "#8d8d8d"
-          colors.blue6 = "#9bb0e0"
-          colors.magenta = "#af9ad6"
-          colors.magenta2 = "#b5a2d9"
-          colors.purple = "#af9ad6"
-          colors.special_char = "#b5a2d9"
+          -- Keep derived semantic colors aligned with the overridden canonical slots.
+          colors.error = colors.red1
+          colors.warning = colors.yellow
+          colors.info = colors.blue2
+          colors.hint = colors.teal
+          colors.todo = colors.blue
+        end,
+      })
+
+      -- NOTE: vantablack.nvim currently exposes on_highlights in config defaults
+      -- but does not apply it internally, so enforce todo keyword readability here.
+      local group = vim.api.nvim_create_augroup("VantasteezTodoHighlights", { clear = true })
+      vim.api.nvim_create_autocmd("ColorScheme", {
+        group = group,
+        pattern = "vantablack",
+        callback = function()
+          vim.api.nvim_set_hl(0, "TodoBgWARN", { fg = "#eaeaea", bg = "NONE", bold = true })
+          vim.api.nvim_set_hl(0, "TodoBgTODO", { fg = "#eaeaea", bg = "NONE", bold = true })
+          vim.api.nvim_set_hl(0, "TodoBgFIX", { fg = "#eaeaea", bg = "NONE", bold = true })
+          vim.api.nvim_set_hl(0, "TodoBgHACK", { fg = "#eaeaea", bg = "NONE", bold = true })
+          vim.api.nvim_set_hl(0, "TodoBgNOTE", { fg = "#eaeaea", bg = "NONE", bold = true })
+          vim.api.nvim_set_hl(0, "TodoBgPERF", { fg = "#eaeaea", bg = "NONE", bold = true })
+          vim.api.nvim_set_hl(0, "TodoFgWARN", { fg = "#d8c08a", bold = true })
+          vim.api.nvim_set_hl(0, "TodoFgTODO", { fg = "#8fa8d8", bold = true })
+          vim.api.nvim_set_hl(0, "TodoFgFIX", { fg = "#c995a0", bold = true })
+          vim.api.nvim_set_hl(0, "TodoFgHACK", { fg = "#d4ad86", bold = true })
+          vim.api.nvim_set_hl(0, "TodoFgNOTE", { fg = "#8fbfc8", bold = true })
+          vim.api.nvim_set_hl(0, "TodoFgPERF", { fg = "#b9a3d7", bold = true })
         end,
       })
     end,
--- themes/configs/vantasteez/sketchybar-colors.lua
+++ themes/configs/vantasteez/sketchybar-colors.lua
@@ -1,15 +1,15 @@
 local colors = {
   black = 0xff2a2a2a,
-  white = 0xfff2f2f2,
-  red = 0xffd68392,
-  green = 0xff9dbc7b,
-  blue = 0xff859fd6,
-  yellow = 0xffc8a87a,
-  orange = 0xffdc9d77,
-  magenta = 0xffaf9ad6,
-  grey = 0xff8d8d8d,
+  white = 0xffeaeaea,
+  red = 0xffc995a0,
+  green = 0xffa3c8a0,
+  blue = 0xff8fa8d8,
+  yellow = 0xffd8c08a,
+  orange = 0xffd4ad86,
+  magenta = 0xffb9a3d7,
+  grey = 0xff8a8a8a,
   transparent = 0x00000000,
-  highlight = 0x33859fd6,
+  highlight = 0x338fa8d8,
   bg0 = 0xff0d0d0d,
   bg1 = 0xff1a1a1a,
   bg2 = 0xff2a2a2a,
@@ -17,12 +17,12 @@ local colors = {
 
 colors.bar = {
   bg = colors.bg0,
-  border = 0xff87bcdc,
+  border = 0xff8fbfc8,
 }
 
 colors.popup = {
   bg = colors.bg0,
-  border = 0xff87bcdc,
+  border = 0xff8fbfc8,
 }
 
 return colors
